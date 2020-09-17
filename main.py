dab52d18042c150e01dfb3cc0d21e40b43c37d1d
smisachsen <stian.m.isachsen@gmail.com>
2020-09-17 14:50:28 +0200

    changed agent definition to main.py

--- main.py
+++ main.py
@@ -5,28 +5,73 @@ import datetime
 import numpy as np
 import json
 import sys
+import argparse
 
 from utils.save_utils import get_new_folder
 
 start_time = datetime.datetime.now()
 
-num_episodes = int(sys.argv[1])
-agent_path = "agents/ppo.json"
-agent_savename = "ppo"
+parser = argparse.ArgumentParser()
+parser.add_argument("--num_episodes", type = int, required = True)
+parser.add_argument("--num_parallel", type = int, required = True)
+
+args = parser.parse_args()
+
+num_episodes = args.num_episodes
+num_parallel = args.num_parallel
+
+network = network = [dict(type='dense', size=512), dict(type='dense', size=512)]
 
 env = SinglePendulumContinous(pendulum_force = 0.1)
-agent = Agent.create(agent = agent_path, environment = env)
-runner = Runner(agent = agent, environment = env)
+agent = Agent.create(
+    agent = "ppo",
+    environment = env,
+
+    network= network,
+    update_frequency = 20,
+    batch_size = 20,
+    learning_rate = 0.001,
+    discount = 0.999,
+
+    critic_network=network,
+    critic_optimizer=dict(
+        type='multi_step', num_steps=5,
+        optimizer=dict(type='adam', learning_rate=1e-3)
+    ),
+
+    entropy_regularization=0.01,
+    parallel_interactions=num_parallel
+)
 
 
-runner.run(num_episodes = num_episodes)
-runner.run(num_episodes = 1, evaluation = True)
+
+#agent_path = "agents/ppo.json"
+agent_savename = "ppo"
+foldername = "single_pendulum_results"
+new_folder_path = get_new_folder(foldername)
+
+
+#agent = Agent.create(agent = agent_path, environment = env)
+
+if num_parallel > 1:
+    envs = [SinglePendulumContinous(pendulum_force = 0.1) for _ in range(num_parallel)]
+    runner = Runner(agent=agent, environments=envs, num_parallel=num_parallel)
+    runner.run(num_episodes = num_episodes)
+else:
+    runner = Runner(agent=agent, environment=env)
+    runner.run(num_episodes = num_episodes)
+
+
+agent.parallel_interactions = 1
+env = env if num_parallel == 1 else envs[0]
+eval_runner = Runner(agent=agent, environment=env)
+eval_runner.run(num_episodes=1, evaluation = True)
+
 
 
 end_time = datetime.datetime.now()
 
-foldername = "single_pendulum_results"
-new_folder_path = get_new_folder(foldername)
+
 
 run_info_txt_path = os.path.join(new_folder_path, "run_info.txt")
 results_csv_path = os.path.join(new_folder_path, "results.csv")
